800eaa34e56b2e4bc0dd0bab89ae64aa051ceaf8
PaPi <89279156+Mesrine67@users.noreply.github.com>
2024-05-23 22:08:14 +0200

    add redesign

---
+++ esx_multicharacter/html/locales/fr.js
@@ -0,0 +1,8 @@
+const translate = new Object();
+
+translate.name = "Nom";
+translate.job = "Métier n°1";
+translate.bank = "Banque";
+translate.money = "Argent";
+translate.gender = "Sexe";
+translate.dob = "Date de naissance";
